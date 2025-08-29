a886e17c02ffb4830d416dd7fb8af4096d136481
MateoRomero08 <omromeroc@academia.usbbog.edu.co>
2025-08-29 01:58:57 +0000

    feat: módulo de carga en SQLite como data lake

---
+++ load/load_data.py
@@ -0,0 +1,13 @@
+import sqlite3
+import pandas as pd
+
+def load_to_sqlite(df: pd.DataFrame, db_name="data_lake.db", table="raw_data"):
+    """
+    CARGAR los datos sin modificar a una base SQLite (data lake local).
+    - df: DataFrame que contiene los datos extraídos
+    - db_name: nombre de la base de datos (por defecto 'data_lake.db')
+    - table: tabla destino donde se guardarán los datos crudos
+    """
+    conn = sqlite3.connect(db_name)
+    df.to_sql(table, conn, if_exists="replace", index=False)
+    conn.close()
